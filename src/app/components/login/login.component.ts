60f608ae7a102582b588879b060b20a5e443bcee
lordofinterface <zakharovvlad.o@gmail.com>
2018-07-03 18:38:36 +0300

    add adding usermodel in store

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { UserService } from "../../services/user/user.service";
-import { Store, select } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { Observable } from 'rxjs';
+import { Store } from "@ngrx/store";
+import { Subscription } from "rxjs";
+import { AddUser } from "../../store/actions";
 
 @Component({
   selector: "app-login",
@@ -14,15 +14,12 @@ export class LoginComponent implements OnInit {
   private userModel: {};
   private _storeSubscription = new Subscription();
 
-  constructor(
-    private _userService: UserService,
-    private _store: Store<any>
-  ) {}
+  constructor(private _userService: UserService, private _store: Store<any>) {}
 
   ngOnInit() {
     this._storeSubscription = this._store.subscribe(state => {
-      this.userModel = state
-    })
+      this.userModel = state;
+    });
   }
 
   loginSubmit(loginForm: NgForm) {
@@ -39,11 +36,16 @@ export class LoginComponent implements OnInit {
           password1: registerForm.value.password1
         })
         .subscribe(res => {
-          this._userService.getUserInfo().subscribe(profile => {
-            console.log('====================================');
-            console.log(profile);
-            console.log('====================================');
-          })
+          this._userService.getProfiles().subscribe(profiles => {
+            if (profiles.length > 1) {
+              const currentProfile = profiles.filter(profile => {
+                profile.username === registerForm.value.email;
+              });
+              this._store.dispatch(new AddUser(currentProfile[0]));
+            } else {
+              this._store.dispatch(new AddUser(profiles[0]));
+            }
+          });
         });
     }
   }
