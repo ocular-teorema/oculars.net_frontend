19913ad055a4efd0c7c415d1e61aa068970f03fd
lordofinterface <zakharovvlad.o@gmail.com>
2018-07-03 18:38:14 +0300

    add new methods

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpService } from "../http/http.service";
 import { TokenService } from "../token/token.service";
 import { Observable } from "rxjs";
-import { tap } from 'rxjs/operators';
-import { UserModule } from './user.type';
+import { tap } from "rxjs/operators";
+import { UserModule } from "./user.type";
 
 const REST_AUTH = "/rest-auth/";
 const REGISTRATION = REST_AUTH + "registration/";
-const PROFILE = '/profile/';
+const LOGOUT = REST_AUTH + "logout/";
+const LOGIN = REST_AUTH + "login/";
+const PROFILE = "/profile/";
 
 @Injectable({
   providedIn: "root"
@@ -18,13 +20,29 @@ export class UserService implements UserModule.IUserService {
     private _tokenService: TokenService
   ) {}
 
-  registerUser(data: UserModule.IUser): Observable<UserModule.IToken> {
+  registerUser(data: UserModule.ILogin): Observable<UserModule.IToken> {
     return this._httpService.postData(REGISTRATION, data).pipe(
-      tap(res => {this._tokenService.setToken(res.key)})
-    )
+      tap(res => {
+        this._tokenService.setToken(res.key);
+      })
+    );
   }
 
-  getUserInfo(): Observable<UserModule.IUser> {
-    return this._httpService.getData(PROFILE)
+  getProfiles(): Observable<UserModule.IUser[]> {
+    return this._httpService.getData(PROFILE);
+  }
+
+  login(data: UserModule.ILogin): Observable<UserModule.IToken> {
+    return this._httpService.postData(LOGIN, data).pipe(
+      tap(res => {
+        this._tokenService.setToken(res.key);
+      })
+    );
+  }
+
+  logout(): Observable<any> {
+    return this._httpService
+      .postData(LOGOUT)
+      .pipe(tap(() => this._tokenService.deleteToken()));
   }
 }
