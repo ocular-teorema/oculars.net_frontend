46878aa3ceba4b1ae4be9326891605211efa10e8
lordofinterface <zakharovvlad.o@gmail.com>
2018-07-04 12:21:54 +0300

    change routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { LoginComponent } from "./components/login/login.component";
-import { ProfileComponent } from "./components/profile/profile.component";
-import { LkComponent } from "./components/lk/lk.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LkComponent } from './components/lk/lk.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/lk", pathMatch: "full" },
-  { path: "login", component: LoginComponent },
-  { path: "profile", component: ProfileComponent },
-  { path: "lk", component: LkComponent }
+  { path: '', redirectTo: 'account', pathMatch: 'full' },
+  {
+    path: 'account',
+    children: [
+      { path: '', redirectTo: 'lk', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'lk', component: LkComponent }
+    ]
+  }
 ];
 
 @NgModule({
