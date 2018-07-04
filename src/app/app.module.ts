c46a3a6601df5817bf4651490e13d715d1fc4f51
lordofinterface <zakharovvlad.o@gmail.com>
2018-07-04 17:25:53 +0300

    add reset component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TokenService } from './services/token/token.service';
 import { HttpClientModule } from '@angular/common/http';
 import { reducers } from './store';
 import { AuthGuardService } from './services/auth-guard/auth-guard.service';
+import { ResetComponent } from './components/reset/reset.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { AuthGuardService } from './services/auth-guard/auth-guard.service';
     LoginComponent,
     HeaderComponent,
     ProfileComponent,
-    LkComponent
+    LkComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
