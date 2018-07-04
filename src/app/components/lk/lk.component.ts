c106f337dcfc5a752b738a0e36296f638271bd93
lordofinterface <zakharovvlad.o@gmail.com>
2018-07-04 14:05:54 +0300

    finish lk

--- src/app/components/lk/lk.component.ts
+++ src/app/components/lk/lk.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { UserModule } from '../../services/user/user.type';
+import { UserService } from '../../services/user/user.service';
+import { AddUser } from '../../store/actions';
 
 @Component({
   selector: 'app-lk',
@@ -10,6 +12,7 @@ import { UserModule } from '../../services/user/user.type';
 })
 export class LkComponent implements OnInit, OnDestroy {
   private _storeSubscription = new Subscription();
+  private _userSubscription = new Subscription();
   public userModel: UserModule.IUser;
   public camList = {
     s: 0,
@@ -17,7 +20,7 @@ export class LkComponent implements OnInit, OnDestroy {
     a: 0
   };
 
-  constructor(private _store: Store<any>) {}
+  constructor(private _store: Store<any>, private _user: UserService) {}
 
   ngOnInit() {
     this._storeSubscription = this._store.select('common').subscribe(state => {
@@ -33,7 +36,28 @@ export class LkComponent implements OnInit, OnDestroy {
     this.camList[type] -= 1;
   }
 
+  public saveCameras(): void {
+    let cam_list = {};
+    Object.keys(this.userModel.cam_list).forEach(type => {
+      cam_list[type] = this.camList[type] + this.userModel.cam_list[type];
+    });
+    this._userSubscription = this._user
+      .changeProfile({
+        id: this.userModel.id,
+        cam_list
+      })
+      .subscribe(profile => {
+        this._store.dispatch(new AddUser(profile));
+        this.camList = {
+          s: 0,
+          f: 0,
+          a: 0
+        };
+      });
+  }
+
   ngOnDestroy() {
     this._storeSubscription.unsubscribe();
+    this._userSubscription.unsubscribe();
   }
 }
